7afec7623c1e4da77dfee4c5050a9c9c1a5032da
Neil Mitchell <ndmitchell@gmail.com>
2019-02-03 21:54:47 +0000

    Update install-stack.sh

--- install-stack.sh
+++ install-stack.sh
@@ -457,9 +457,10 @@ do_os() {
     "FreeBSD")
       do_freebsd_install
       ;;
-    MSYS_NT-*)
+    MINGW64_NT|MSYS_NT-*)
       do_windows_install
       ;;
+    "MINGW64_NT"
     *)
       die "Sorry, this installer does not support your operating system: $(uname).
 See http://docs.haskellstack.org/en/stable/install_and_upgrade/"
